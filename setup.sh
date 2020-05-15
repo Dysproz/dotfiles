cac66ed9d0b1f99c91ff2ec9ac148170b744ddcc
Szymon Piotr Krasuski <krasuski.szymon.piotr@gmail.com>
2020-05-15 11:34:13 +0200

    [tmux] System clipboard + some plugins (#4)

--- setup.sh
+++ setup.sh
@@ -5,11 +5,27 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Download VIM Plugged
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+# Download tpm for tmux
+mkdir ~/.tmux
+mkdir ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Set links
 ln ${DIR}/.vimrc ~/.vimrc
 ln ${DIR}/.tmux.conf ~/.tmux.conf
 mkdir ~/.config
 mkdir ~/.config/termite
 ln ${DIR}/.config/termite/config ~/.config/termite/config
+
+# Install vim plugins
+vim +'PlugInstall --sync' +qa
+
+# Install tmux plugins
+# start a server but don't attach to it
+tmux start-server
+# create a new session but don't attach to it either
+tmux new-session -d
+# install the plugins
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+# killing the server is not required, I guess
+tmux kill-server
 
