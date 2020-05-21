5c1334f1bc3db9302594b11f367cbce45a88e669
Dysproz <krasuski.szymon.piotr@gmail.com>
2020-05-21 19:49:02 +0200

    Fix termux startup

--- bash_configs.sh
+++ bash_configs.sh
@@ -1,11 +1,7 @@
-# Make vi redirect to vim
-alias vi=vim
-
-# Export EDITOR to vi
-export EDITOR=vim
-
 # Start tmux on every shell login
-[[ $TERM != "screen" ]] && exec tmux
+if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+  exec tmux
+fi
 # Make vi redirect to vim
 alias vi=vim
 alias svi='sudo vim'
@@ -13,13 +9,6 @@ alias svi='sudo vim'
 # Export EDITOR to vi
 export EDITOR=vim
 export VISUAL=vim
-
-# Start tmux on every shell login
-[[ $TERM != "screen" ]] && exec tmux
-
-# Causes bash to append to history instead of overwriting it so if you start a new terminal, you have old session history
-shopt -s histappend
-PROMPT_COMMAND='history -a'
 
 # Make alias for repeating command with sudo
 alias s!='sudo !!'
@@ -43,30 +32,6 @@ alias mkgz='tar -cvzf'
 alias untar='tar -xvf'
 alias unbz2='tar -xvjf'
 alias ungz='tar -xvzf'
-
-# Extracts any archive(s) (if unp isn't installed)
-extract () {
-    for archive in $*; do
-            if [ -f $archive ] ; then
-                        case $archive in
-                            *.tar.bz2)   tar xvjf $archive    ;;
-                            *.tar.gz)    tar xvzf $archive    ;;
-                            *.bz2)       bunzip2 $archive     ;;
-                            *.rar)       rar x $archive       ;;
-                            *.gz)        gunzip $archive      ;;
-                            *.tar)       tar xvf $archive     ;;
-                            *.tbz2)      tar xvjf $archive    ;;
-                            *.tgz)       tar xvzf $archive    ;;
-                            *.zip)       unzip $archive       ;;
-                            *.Z)         uncompress $archive  ;;
-                            *.7z)        7z x $archive        ;;
-                            *)           echo "don't know how to extract '$archive'..." ;;
-                        esac
-            else
-                echo "'$archive' is not a valid file!"
-            fi
-    done
-}
 
 eval $(ssh-agent -s) &> /dev/null
 ssh-add ~/.ssh/id_rsa 2> /dev/null
